6ae35e3fbd1f09d899051d2f603486bc620275ce
detective0922 <detective0922@hotmail.com>
2017-01-03 01:35:26 +0800

    MaximumDepthofBinaryTree

--- LeetCode/src/MaximumDepthofBinaryTree.java
+++ LeetCode/src/MaximumDepthofBinaryTree.java
@@ -14,11 +14,13 @@ public class MaximumDepthofBinaryTree {
 		if (root == null) {
 			return 0;
 		}
-		
-		if (root.) {
+		int depth = 0;
+		if (root.left==null&&root.right==null) {
 			
-		} else {
-
+		}
+		
+		if (root.left !=null) {
+			
 		}
 
 	}
