b1642ba0e4ae49bd4071a5017691ee2f37c4c818
detective0922 <detective0922@hotmail.com>
2017-01-10 10:57:47 +0800

    MoveZeroes

--- LeetCode/src/MoveZeroes.java
+++ LeetCode/src/MoveZeroes.java
@@ -32,9 +32,11 @@ public class MoveZeroes {
 		int j = 0;
 		for (int i = 0; i < nums.length; i++) {
 			if (nums[i] != 0) {
-				int temp = nums[j];
-				nums[j] = nums[i];
-				nums[i] = temp;
+				if (i > j) {
+					int temp = nums[j];
+					nums[j] = nums[i];
+					nums[i] = temp;
+				}
 				j++;
 			}
 			
