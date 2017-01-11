ec4d5a949697f2267a8a21c0ad558ddfba98b4dd
detective0922 <detective0922@hotmail.com>
2017-01-11 16:42:43 +0800

    IntersectionofTwoArrays

--- LeetCode/src/IntersectionofTwoArrays.java
+++ LeetCode/src/IntersectionofTwoArrays.java
@@ -32,11 +32,11 @@ public class IntersectionofTwoArrays {
 	public int[] intersection(int[] nums1, int[] nums2) {
 		Arrays.sort(nums1);
 		Arrays.sort(nums2);
-		
+
 		List<Integer> intersectionList = new ArrayList<Integer>();
 		int i = 0;
 		int j = 0;
-		
+
 		while (i < nums1.length && j < nums2.length) {
 			if (nums1[i] > nums2[j]) {
 				j++;
@@ -50,7 +50,7 @@ public class IntersectionofTwoArrays {
 				j++;
 			}
 		}
-		
+
 		int[] intersection = new int[intersectionList.size()];
 		int k = 0;
 		for (int num : intersectionList) {
