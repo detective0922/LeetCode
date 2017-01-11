674ceb465d9e92b4571ccdeffd9849a155e183b5
detective0922 <detective0922@hotmail.com>
2017-01-11 14:49:57 +0800

    SumofLeftLeaves

--- LeetCode/src/SumofLeftLeaves.java
+++ LeetCode/src/SumofLeftLeaves.java
@@ -19,15 +19,15 @@ There are two left leaves in the binary tree, with values 9 and 15 respectively.
 public class SumofLeftLeaves {
 	
 	public static void main(String[] args) {
-		TreeNode root = new TreeNode(3);
-		TreeNode two = new TreeNode(9);
-		TreeNode three = new TreeNode(20);
-		TreeNode four = new TreeNode(15);
-		TreeNode five = new TreeNode(7);
+		TreeNode root = new TreeNode(1);
+		TreeNode two = new TreeNode(2);
+		TreeNode three = new TreeNode(3);
+		TreeNode four = new TreeNode(4);
+		TreeNode five = new TreeNode(5);
 		root.left = two;
 		root.right = three;
 		two.left = four;
-		three.right = five;
+		two.right = five;
 		System.out.println(new SumofLeftLeaves().sumOfLeftLeaves(root));
 	}
 	
@@ -37,11 +37,11 @@ public class SumofLeftLeaves {
 			return 0;
 		}
 
-		if (root.left == null) {
-			return 0;
+		if (root.left == null && root.right == null) {
+			return root.val;
 		}
 
-		return root.left.val + sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right);
+		return sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right);
 		/*int leftsum = 0;
 		int rightsum = 0;
 		if (root.left != null) {
