605f6fecf562dc5db30a3125b8b23d51e8d4d7ff
detective0922 <detective0922@hotmail.com>
2017-01-13 15:45:50 +0800

    FindTheDifference

--- LeetCode/src/FindTheDifference.java
+++ LeetCode/src/FindTheDifference.java
@@ -56,6 +56,20 @@ public class FindTheDifference {
 			}
 		}
 		return '0';
+		
+		//a better solution
+		/*int x= 0;
+	    
+	    for(int i=0; i<s.length();i++){
+	        x= x ^ s.charAt(i);
+	    }
+	    
+	    for(int i = 0; i<t.length(); i++){
+	        x= x ^ t.charAt(i);
+	    }
+	    
+	    return (char) x;*/
+
 
 	}
 
