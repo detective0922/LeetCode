ebd83dbf638fbc507cd3ea0fed14da688b442458
detective0922 <detective0922@hotmail.com>
2017-01-13 16:58:41 +0800

    ValidAnagram

--- LeetCode/src/ValidAnagram.java
+++ LeetCode/src/ValidAnagram.java
@@ -19,6 +19,9 @@ Follow up:
 
 public class ValidAnagram {
 	public boolean isAnagram(String s, String t) {
+		if (s.length() != t.length()) {
+			return false;
+		}
 		
 		char[] sChars = s.toCharArray();
 		char[] tChars = t.toCharArray();
