f04474112b12c79a2839e9bd0e51c341611a171e
detective0922 <detective0922@hotmail.com>
2017-01-14 12:16:49 +0800

    LongestPalindrome

--- LeetCode/src/LongestPalindrome.java
+++ LeetCode/src/LongestPalindrome.java
@@ -23,6 +23,10 @@ One longest palindrome that can be built is "dccaccd", whose length is 7.
 
 public class LongestPalindrome {
 	public int longestPalindrome(String s) {
+		int[] table = new int[256];
+		for (int i = 0; i < s.length(); i++) {
+			table[s.charAt(i)]++;
+		}
 
 	}
 
