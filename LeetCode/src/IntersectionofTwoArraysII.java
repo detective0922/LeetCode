56eb72439e48309870ffdb43237710d8fbdf23b0
detective0922 <detective0922@hotmail.com>
2017-01-14 13:20:52 +0800

    IntersectionofTwoArraysII

--- LeetCode/src/IntersectionofTwoArraysII.java
+++ LeetCode/src/IntersectionofTwoArraysII.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /*
 350. Intersection of Two Arrays II
@@ -24,6 +27,31 @@ What if elements of nums2 are stored on disk, and the memory is limited such tha
 
 public class IntersectionofTwoArraysII {
 	public int[] intersect(int[] nums1, int[] nums2) {
+		Arrays.sort(nums1);
+		Arrays.sort(nums2);
+
+		List<Integer> intersectionList = new ArrayList<Integer>();
+		int i = 0;
+		int j = 0;
+
+		while (i < nums1.length && j < nums2.length) {
+			if (nums1[i] > nums2[j]) {
+				j++;
+			} else if (nums1[i] < nums2[j]) {
+				i++;
+			} else {
+				intersectionList.add(nums1[i]);			
+				i++;
+				j++;
+			}
+		}
+
+		int[] intersection = new int[intersectionList.size()];
+		int k = 0;
+		for (int num : intersectionList) {
+			intersection[k++] = num;
+		}
+		return intersection;
 
 	}
 
