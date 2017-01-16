7d103fb4b91c6653d5eeededf15392e97f5296a8
detective0922 <detective0922@hotmail.com>
2017-01-16 15:24:04 +0800

    ReverseLinkedList

--- LeetCode/src/ReverseLinkedList.java
+++ LeetCode/src/ReverseLinkedList.java
@@ -8,6 +8,12 @@ Reverse a singly linked list.
 
 public class ReverseLinkedList {
 	public ListNode reverseList(ListNode head) {
+		ListNode newHead = null;
+		while (head != null) {
+			ListNode temp = head.next;
+			
+			head = temp;
+		}
 
 	}
 
