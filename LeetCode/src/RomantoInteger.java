dd4a10d918939d6ec78375fca471f3b04f68d533
detective0922 <detective0922@hotmail.com>
2017-01-16 14:28:24 +0800

    RomantoInteger

--- LeetCode/src/RomantoInteger.java
+++ LeetCode/src/RomantoInteger.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+import java.util.Map;
 
 /*
 
@@ -12,9 +14,38 @@ Input is guaranteed to be within the range from 1 to 3999.
 
 public class RomantoInteger {
 	public int romanToInt(String s) {
-		char[] romans = { 'I', 'V', 'X', 'L', 'C', 'D', 'M' };
-		int[] romanToInt = { 1, 5, 10, 50, 100, 500, 1000 };
-
+		/*char[] romans = { 'I', 'V', 'X', 'L', 'C', 'D', 'M' };
+		int[] romanToInt = { 1, 5, 10, 50, 100, 500, 1000 };*/
+		
+		if (s == null) {
+			return -1;
+		}
+		
+		int[] romanToInt = new int[256];
+		romanToInt['I'] = 1;
+		romanToInt['V'] = 5;
+		romanToInt['X'] = 10;
+		romanToInt['L'] = 50;
+		romanToInt['C'] = 100;
+		romanToInt['D'] = 500;
+		romanToInt['M'] = 1000;
+		
+		int intNum = 0;
+		for (int i = 0; i < s.length() - 1; i++) {
+			char c = s.charAt(i);
+			if (c == 0) {
+				return -1;
+			}
+			if (romanToInt[s.charAt(i)]>=romanToInt[s.charAt(i+1)]) {
+				intNum += romanToInt[s.charAt(i)];
+			} else {
+				intNum -= romanToInt[s.charAt(i)];
+			}
+		}
+		
+		intNum += romanToInt[s.charAt(s.length() - 1)];
+		return intNum;
+		
 	}
 
 }
