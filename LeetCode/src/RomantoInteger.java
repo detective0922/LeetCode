03d654650c6ada642e614f3a4db53b797ab16140
detective0922 <detective0922@hotmail.com>
2017-01-16 14:18:04 +0800

    RomantoInteger

--- LeetCode/src/RomantoInteger.java
+++ LeetCode/src/RomantoInteger.java
@@ -12,6 +12,8 @@ Input is guaranteed to be within the range from 1 to 3999.
 
 public class RomantoInteger {
 	public int romanToInt(String s) {
+		char[] romans = { 'I', 'V', 'X', 'L', 'C', 'D', 'M' };
+		int[] romanToInt = { 1, 5, 10, 50, 100, 500, 1000 };
 
 	}
 
