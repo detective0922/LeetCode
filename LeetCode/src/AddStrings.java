81a561993b88a05285fecbecb4d818a449ed2224
detective0922 <detective0922@hotmail.com>
2017-01-19 13:20:37 +0800

    AddStrings

--- LeetCode/src/AddStrings.java
+++ LeetCode/src/AddStrings.java
@@ -72,6 +72,18 @@ public class AddStrings {
 		
 		return sumStr.reverse().toString();
 		
+		// a better solution
+		/*StringBuilder sb = new StringBuilder();
+        int carry = 0;
+        for(int i = num1.length() - 1, j = num2.length() - 1; i >= 0 || j >= 0 || carry == 1; i--, j--){
+            int x = i < 0 ? 0 : num1.charAt(i) - '0';
+            int y = j < 0 ? 0 : num2.charAt(j) - '0';
+            sb.append((x + y + carry) % 10);
+            carry = (x + y + carry) / 10;
+        }
+        return sb.reverse().toString();*/
+
+		
 		/*int[] num1Array = strToArray(num1);
 		int[] num2Array = strToArray(num2);
 		
