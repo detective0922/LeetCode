3caed725451751dde85c6ab31c2763815c1e08f8
detective0922 <detective0922@hotmail.com>
2017-01-19 11:26:43 +0800

    AddStrings

--- LeetCode/src/AddStrings.java
+++ LeetCode/src/AddStrings.java
@@ -17,7 +17,7 @@ Note:
 public class AddStrings {
 	
 	public static void main(String[] args) {
-		System.out.println(new AddStrings().addStrings("999999", "88888"));
+		System.out.println(new AddStrings().addStrings("999999", "1"));
 	}
 	
 	public String addStrings(String num1, String num2) {
