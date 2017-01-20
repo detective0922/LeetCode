c58b792dce66a3a5bcdd34ebd059b373ac8ece3b
detective0922 <detective0922@hotmail.com>
2017-01-20 17:01:19 +0800

    PowerofThree

--- LeetCode/src/PowerofThree.java
+++ LeetCode/src/PowerofThree.java
@@ -11,8 +11,20 @@ Follow up:
 */
 
 public class PowerofThree {
+	
+	public static void main(String[] args) {
+		System.out.println(new PowerofThree().isPowerOfThree(2147483646));
+	}
+	
 	public boolean isPowerOfThree(int n) {
-
+		if (n == 1) {
+			return true;
+		}
+		long num = 3;
+		while (num < n) {
+			num *= 3;
+		}
+		return num == n;
 	}
 
 }
