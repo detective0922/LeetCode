884d9fd59c498477f34354ef8fcd8c8e1c3072e5
detective0922 <detective0922@hotmail.com>
2017-01-21 13:24:52 +0800

    PowerofTwo

--- LeetCode/src/PowerofTwo.java
+++ LeetCode/src/PowerofTwo.java
@@ -9,6 +9,9 @@ Given an integer, write a function to determine if it is a power of two.
 
 public class PowerofTwo {
 	public boolean isPowerOfTwo(int n) {
+		if (n == 1) {
+			return true;
+		}
 
 	}
 
