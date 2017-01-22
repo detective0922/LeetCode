63a0ac72294ea331c0b955f86cca629f6aa6ec11
detective0922 <detective0922@hotmail.com>
2017-01-22 15:21:01 +0800

    BestTimetoBuyandSellStock

--- LeetCode/src/BestTimetoBuyandSellStock.java
+++ LeetCode/src/BestTimetoBuyandSellStock.java
@@ -30,6 +30,17 @@ In this case, no transaction is done, i.e. max profit = 0.
 
 public class BestTimetoBuyandSellStock {
 	public int maxProfit(int[] prices) {
+		int maxProfit = 0;
+		for (int i = 0; i < prices.length; i++) {
+			int buy = prices[i];
+			for (int j = i + 1; j < prices.length; j++) {
+				int profit = prices[j] - buy;
+				if (profit > maxProfit) {
+					maxProfit = profit;
+				}
+			}
+		}
+		return maxProfit;
 
 	}
 
