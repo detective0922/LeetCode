9d8c19dfe37facdb77659f163d04d84c7b28fe8e
detective0922 <detective0922@hotmail.com>
2017-01-23 18:38:58 +0800

    HammingDistance

--- LeetCode/src/HammingDistance.java
+++ LeetCode/src/HammingDistance.java
@@ -28,7 +28,7 @@ public class HammingDistance {
 	public int hammingDistance(int x, int y) {
 		int count = 0;
 		while (x > 0 || y > 0) {
-			if ((x & 1) == (y & 1)) {
+			if ((x & 1) != (y & 1)) {
 				count++;
 			}
 			x >>= 1;
