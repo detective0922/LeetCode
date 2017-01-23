663cc4d2a3238293809ea77ef1dee4261b603abe
detective0922 <detective0922@hotmail.com>
2017-01-23 18:38:21 +0800

    HammingDistance

--- LeetCode/src/HammingDistance.java
+++ LeetCode/src/HammingDistance.java
@@ -26,7 +26,15 @@ The above arrows point to positions where the corresponding bits are different.
 
 public class HammingDistance {
 	public int hammingDistance(int x, int y) {
-
+		int count = 0;
+		while (x > 0 || y > 0) {
+			if ((x & 1) == (y & 1)) {
+				count++;
+			}
+			x >>= 1;
+			y >>= 1;
+		}
+		return count;
 	}
 
 }
