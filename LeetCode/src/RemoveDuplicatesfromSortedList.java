b8a80eafe02222989c9b0745c789f792d09bc89a
detective0922 <detective0922@hotmail.com>
2017-01-24 15:28:57 +0800

    RemoveDuplicatesfromSortedList

--- LeetCode/src/RemoveDuplicatesfromSortedList.java
+++ LeetCode/src/RemoveDuplicatesfromSortedList.java
@@ -11,6 +11,20 @@ For example,
 */
 
 public class RemoveDuplicatesfromSortedList {
+	
+	public static void main(String[] args) {
+		ListNode root = new ListNode(1);
+		ListNode two = new ListNode(1);
+		ListNode three = new ListNode(2);
+		ListNode four = new ListNode(3);
+		ListNode five = new ListNode(3);
+		root.next = two;
+		two.next = three;
+		three.next = four;
+		four.next = five;
+		System.out.println(new RemoveDuplicatesfromSortedList().deleteDuplicates(root));
+	}
+	
 	public ListNode deleteDuplicates(ListNode head) {
 		
 		ListNode currentNode = head;
