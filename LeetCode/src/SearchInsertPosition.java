c60b8ef3251feea5c91c9972da39490e0c255259
detective0922 <detective0922@hotmail.com>
2017-01-29 15:08:59 +0800

    SearchInsertPosition

--- LeetCode/src/SearchInsertPosition.java
+++ LeetCode/src/SearchInsertPosition.java
@@ -21,11 +21,15 @@ public class SearchInsertPosition {
     public int searchInsert(int[] nums, int target) {
         int index = -1;
         for (int i = 0; i < nums.length; i++) {
-            if(nums[i]>target){
-
+            if (nums[i] > target) {
+                index = i;
+                break;
             }
-
         }
+        if (index == -1) {
+            index = nums.length;
+        }
+        return index;
 
     }
 }
