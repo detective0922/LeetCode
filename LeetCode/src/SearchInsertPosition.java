42a7357edb5972b9847e71dc1a79fb09188d9df2
detective0922 <detective0922@hotmail.com>
2017-01-29 15:06:53 +0800

    SearchInsertPosition

--- LeetCode/src/SearchInsertPosition.java
+++ LeetCode/src/SearchInsertPosition.java
@@ -19,7 +19,11 @@ Here are few examples.
 
 public class SearchInsertPosition {
     public int searchInsert(int[] nums, int target) {
+        int index = -1;
         for (int i = 0; i < nums.length; i++) {
+            if(nums[i]>target){
+
+            }
 
         }
 
