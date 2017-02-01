8a02e7b09e9510c079901cbafb96a17a380a5a3b
detective0922 <detective0922@hotmail.com>
2017-02-01 16:55:45 +0800

    Numberof1Bits

--- LeetCode/src/Numberof1Bits.java
+++ LeetCode/src/Numberof1Bits.java
@@ -11,8 +11,11 @@ public class Numberof1Bits {
     // you need to treat n as an unsigned value
     public int hammingWeight(int n) {
         int count = 0;
-
-        return  count;
+        while (n != 0) {
+            count += n& 1;
+            n>>=1;
+        }
+        return count;
 
     }
 }
