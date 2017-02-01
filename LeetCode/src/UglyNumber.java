57b9a0b75dd209c5a9a681e80db4067a8b0634ac
detective0922 <detective0922@hotmail.com>
2017-02-01 17:19:13 +0800

    UglyNumber

--- LeetCode/src/UglyNumber.java
+++ LeetCode/src/UglyNumber.java
@@ -13,17 +13,17 @@ Note that 1 is typically treated as an ugly number.
 */
 public class UglyNumber {
     public boolean isUgly(int num) {
-        while(num>0){
-            if (num%5==0){
-                num /=5;
-            }else if (num%3==0){
-                num /=3;
-            }else if (num%2==0){
-                num /=2;
+        while (num > 0) {
+            if (num % 5 == 0) {
+                num /= 5;
+            } else if (num % 3 == 0) {
+                num /= 3;
+            } else if (num % 2 == 0) {
+                num /= 2;
             } else {
-                return  false;
+                return false;
             }
         }
-        return  true;
+        return true;
     }
 }
