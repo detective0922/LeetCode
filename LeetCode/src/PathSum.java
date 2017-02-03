d2e5f59801b7eab05304e00b57883c8de14d7c7c
detective0922 <detective0922@hotmail.com>
2017-02-04 00:48:19 +0800

    hasPathSum

--- LeetCode/src/PathSum.java
+++ LeetCode/src/PathSum.java
@@ -21,7 +21,7 @@ return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
 
 public class PathSum {
 	public boolean hasPathSum(TreeNode root, int sum) {
-
+		return false;
 	}
 
 }
