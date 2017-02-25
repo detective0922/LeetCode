bcd509d1b66c7ead517e3ec427534269a75190c7
detective0922 <detective0922@hotmail.com>
2017-02-26 01:27:09 +0800

    BinaryTreeLevelOrderTraversalII

--- LeetCode/src/BinaryTreeLevelOrderTraversalII.java
+++ LeetCode/src/BinaryTreeLevelOrderTraversalII.java
@@ -1,9 +1,10 @@
 
 /*
 
-121. Best Time to Buy and Sell Stock
+107. Binary Tree Level Order Traversal II
 
-Say you have an array for which the ith element is the price of a given stock on day i.
+Given a binary tree, return the bottom-up level order traversal of its nodes' values. 
+(ie, from left to right, level by level from leaf to root).
 
 If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), 
 design an algorithm to find the maximum profit.
