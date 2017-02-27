c006c7d6daf9c4655267c0e5ba8225a1138f4531
detective0922 <detective0922@hotmail.com>
2017-02-28 02:15:45 +0800

    BinaryTreeLevelOrderTraversalII

--- LeetCode/src/BinaryTreeLevelOrderTraversalII.java
+++ LeetCode/src/BinaryTreeLevelOrderTraversalII.java
@@ -6,27 +6,23 @@
 Given a binary tree, return the bottom-up level order traversal of its nodes' values. 
 (ie, from left to right, level by level from leaf to root).
 
-If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), 
-design an algorithm to find the maximum profit.
-
-Example 1:
-
-Input: [7, 1, 5, 3, 6, 4]
-Output: 5
-
-max. difference = 6-1 = 5 (not 7-1 = 6, as selling price needs to be larger than buying price)
-
-
-
-Example 2:
-
-Input: [7, 6, 4, 3, 1]
-Output: 0
-
-In this case, no transaction is done, i.e. max profit = 0.
+For example:
+Given binary tree [3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its bottom-up level order traversal as:
+[
+  [15,7],
+  [9,20],
+  [3]
+]
 
 
 */
+
 public class BinaryTreeLevelOrderTraversalII {
 
 }
