f4fc4227de92b23dbc5c7a4964f4fcd38d6effc7
detective0922 <detective0922@hotmail.com>
2017-03-13 14:48:55 +0800

    LowestCommonAncestorofaBinarySearchTree

--- LeetCode/src/LowestCommonAncestorofaBinarySearchTree.java
+++ LeetCode/src/LowestCommonAncestorofaBinarySearchTree.java
@@ -1,23 +1,21 @@
 /*
 107. Binary Tree Level Order Traversal II
 
-Given a binary tree, return the bottom-up level order traversal of its nodes' values. 
-(ie, from left to right, level by level from leaf to root).
+Given a binary search tree (BST), find the lowest common ancestor (LCA) of two given nodes in the BST. 
 
-For example:
-Given binary tree [3,9,20,null,null,15,7],
-    3
-   / \
-  9  20
-    /  \
-   15   7
-return its bottom-up level order traversal as:
-[
-  [15,7],
-  [9,20],
-  [3]
-]
+According to the definition of LCA on Wikipedia: ¡°The lowest common ancestor is defined between 
+two nodes v and w as the lowest node in T that has both v and w as descendants (where we allow a node to be a descendant of itself).¡± 
+        _______6______
+       /              \
+    ___2__          ___8__
+   /      \        /      \
+   0      _4       7       9
+         /  \
+         3   5
 
+
+For example, the lowest common ancestor (LCA) of nodes 2 and 8 is 6. 
+Another example is LCA of nodes 2 and 4 is 2, since a node can be a descendant of itself according to the LCA definition.
 
 */
 
