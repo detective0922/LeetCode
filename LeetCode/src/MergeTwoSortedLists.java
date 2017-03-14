54008b17362393e5b6517c556a489e95ea61d964
detective0922 <detective0922@hotmail.com>
2017-03-14 15:58:55 +0800

    MergeTwoSortedLists

--- LeetCode/src/MergeTwoSortedLists.java
+++ LeetCode/src/MergeTwoSortedLists.java
@@ -9,6 +9,15 @@ The new list should be made by splicing together the nodes of the first two list
 
 public class MergeTwoSortedLists {
 	public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+		if (l1 == null && l2 == null) {
+			return null;
+		}
+		if (l1 == null) {
+			return l2;
+		}
+		if (l2 == null) {
+			return l1;
+		}
 
 	}
 }
