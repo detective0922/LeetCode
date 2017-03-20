25a071f1f819bd0a4c92124792f1a7522e25b71b
detective0922 <detective0922@hotmail.com>
2017-03-20 17:20:13 +0800

    PowerofFour

--- LeetCode/src/PowerofFour.java
+++ LeetCode/src/PowerofFour.java
@@ -1,4 +1,15 @@
 
+/*
+342. Power of Four
+
+Given an integer, write a function to determine if it is a power of three. 
+
+Follow up:
+ Could you do it without using any loop / recursion? 
+
+
+*/
+
 public class PowerofFour {
 
 }
