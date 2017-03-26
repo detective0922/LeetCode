0e8344471d38f73d80833d7f4dd08509a7dabcff
detective0922 <detective0922@hotmail.com>
2017-03-26 20:38:20 +0800

    ReverseVowelsofaString

--- LeetCode/src/ReverseVowelsofaString.java
+++ LeetCode/src/ReverseVowelsofaString.java
@@ -1,4 +1,18 @@
 
+/*
+
+345. Reverse Vowels of a String
+
+Write a function that takes a string as input and reverse only the vowels of a string.
+
+Example 1:
+Given s = "hello", return "holle".
+
+Example 2:
+Given s = "leetcode", return "leotcede".
+
+*/
+
 public class ReverseVowelsofaString {
 
 }
