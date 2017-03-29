06f873245abc7d1f84cb4dd20283ebe9c30ad141
detective0922 <detective0922@hotmail.com>
2017-03-29 14:57:10 +0800

    PlusOne

--- LeetCode/src/PlusOne.java
+++ LeetCode/src/PlusOne.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 
 /*
 
@@ -11,6 +13,14 @@ The digits are stored such that the most significant digit is at the head of the
 */
 public class PlusOne {
     public int[] plusOne(int[] digits) {
+        List<Integer> digitList = new ArrayList<Integer>();
+        int digit = (digits[digits.length - 1] + 1) % 10;
+        int carry = (digits[digits.length - 1] + 1) / 10;
+        digitList.add(digit);
+        for (int i = digits.length -2;i>=0;i--){
+            digit = (digits[i] + carry) % 10;
+            carry = (digits[i] + carry) / 10;
+        }
 
     }
 
