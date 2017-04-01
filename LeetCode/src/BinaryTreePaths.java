a10e7d7b99d92d855e2b3947b85f49a89a3bd511
detective0922 <detective0922@hotmail.com>
2017-04-01 11:17:59 +0800

    BinaryTreePaths

--- LeetCode/src/BinaryTreePaths.java
+++ LeetCode/src/BinaryTreePaths.java
@@ -25,25 +25,25 @@ All root-to-leaf paths are:
 public class BinaryTreePaths {
     public List<String> binaryTreePaths(TreeNode root) {
         List<String> pathList = new ArrayList<String>();
-        if (root == null){
+        if (root == null) {
             return pathList;
         }
 
         StringBuilder strBuilder = new StringBuilder();
         strBuilder.append(root.val);
-        path(root.left,strBuilder,pathList);
-        path(root.right,strBuilder,pathList);
+        path(root.left, strBuilder, pathList);
+        path(root.right, strBuilder, pathList);
         return pathList;
     }
 
     public void path(TreeNode node, StringBuilder strBuilder, List<String> pathList) {
-        if (node!=null){
+        if (node != null) {
             strBuilder.append("->").append(node.val);
         } else {
             return;
         }
 
-        if (node.left == null && node.right==null) {
+        if (node.left == null && node.right == null) {
             pathList.add(strBuilder.toString());
         } else {
             path(node.left, strBuilder, pathList);
