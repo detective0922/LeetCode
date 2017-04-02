1fc807b3c5b035d46d67fb15b31668df455678b6
detective0922 <detective0922@hotmail.com>
2017-04-03 06:20:01 +0800

    PascalsTriangleII

--- LeetCode/src/PascalsTriangleII.java
+++ LeetCode/src/PascalsTriangleII.java
@@ -14,9 +14,14 @@ Could you optimize your algorithm to use only O(k) extra space?
 
 */
 public class PascalsTriangleII {
+
+    public static void main(String[] args) {
+        System.out.println(new PascalsTriangleII().getRow(3));
+    }
+
     public List<Integer> getRow(int rowIndex) {
         List<Integer> row = new ArrayList<Integer>();
-        for (int i = 0; i < rowIndex; i++) {
+        for (int i = 0; i < rowIndex + 1; i++) {
             row.add(0, 1);
             for (int j = 1; j < row.size() - 1; j++) {
                 row.set(j, row.get(j) + row.get(j + 1));
