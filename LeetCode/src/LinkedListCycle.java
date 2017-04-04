0916c31ed19783e337f56c8ffb47bdc4bd10b4b5
detective0922 <detective0922@hotmail.com>
2017-04-04 17:39:46 +0800

    LinkedListCycle

--- LeetCode/src/LinkedListCycle.java
+++ LeetCode/src/LinkedListCycle.java
@@ -16,7 +16,8 @@ public class LinkedListCycle {
 
         ListNode walker = head;
         ListNode runner = head;
-        while (walker.next != null && runner != null && runner.next != null) {
+        //while (walker.next != null && runner != null && runner.next != null) {
+        while (runner.next != null && runner.next.next != null) {
             walker = walker.next;
             runner = runner.next.next;
             if (walker == runner) {
