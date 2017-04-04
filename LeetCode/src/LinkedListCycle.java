3ae6165991fa9d9b48facb0688bf6222607a9168
detective0922 <detective0922@hotmail.com>
2017-04-04 17:32:18 +0800

    LinkedListCycle

--- LeetCode/src/LinkedListCycle.java
+++ LeetCode/src/LinkedListCycle.java
@@ -10,11 +10,18 @@ Can you solve it without using extra space?
 
 public class LinkedListCycle {
     public boolean hasCycle(ListNode head) {
+        if (head == null){
+            return false;
+        }
+
         ListNode walker = head;
         ListNode runner = head;
-        while (walker!=null&&runner !=null && runner.next != null) {
+        while (walker != null && runner != null && runner.next != null) {
             walker = walker.next;
             runner = runner.next.next;
+            if (walker==runner){
+                return true;
+            }
         }
         return false;
 
