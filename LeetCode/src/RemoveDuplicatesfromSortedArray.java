3793797232bed8a7bbfb468cde90988c4be994d7
detective0922 <detective0922@hotmail.com>
2017-04-04 19:00:54 +0800

    RemoveDuplicatesfromSortedArray

--- LeetCode/src/RemoveDuplicatesfromSortedArray.java
+++ LeetCode/src/RemoveDuplicatesfromSortedArray.java
@@ -14,7 +14,7 @@ Your function should return length = 2, with the first two elements of nums bein
 public class RemoveDuplicatesfromSortedArray {
     public int removeDuplicates(int[] nums) {
         int j = 0;
-        for (int i = 0;i<nums.length;i++){
+        for (int i = 1;i<nums.length;i++){
             if (nums[i]!=nums[j]){
                 j++;
             }
