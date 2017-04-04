a2549d0cad3f8ffedf84b2de5f5b2589b0cdd9e8
detective0922 <detective0922@hotmail.com>
2017-04-04 19:10:09 +0800

    RemoveDuplicatesfromSortedArray

--- LeetCode/src/RemoveDuplicatesfromSortedArray.java
+++ LeetCode/src/RemoveDuplicatesfromSortedArray.java
@@ -24,8 +24,7 @@ public class RemoveDuplicatesfromSortedArray {
         int j = 0;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != nums[j]) {
-                j++;
-                swap(nums, i, j);
+                swap(nums, i, ++j);
             }
         }
         return j + 1;
