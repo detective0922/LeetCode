e66e89551c683f3a2ef4dc3517f52435a0ce6dd5
detective0922 <detective0922@hotmail.com>
2017-04-05 15:11:12 +0800

    FactorialTrailingZeroes

---
+++ LeetCode/src/FactorialTrailingZeroes.java
@@ -0,0 +1,11 @@
+/*
+
+172. Factorial Trailing Zeroes
+
+Given an integer n, return the number of trailing zeroes in n!.
+
+Note: Your solution should be in logarithmic time complexity.
+
+*/
+public class FactorialTrailingZeroes {
+}
