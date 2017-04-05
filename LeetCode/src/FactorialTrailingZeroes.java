8d24a78e278a8e00ced7c0b31f499b19906265c0
detective0922 <detective0922@hotmail.com>
2017-04-05 16:24:45 +0800

    FactorialTrailingZeroes

--- LeetCode/src/FactorialTrailingZeroes.java
+++ LeetCode/src/FactorialTrailingZeroes.java
@@ -9,12 +9,12 @@ Note: Your solution should be in logarithmic time complexity.
 */
 public class FactorialTrailingZeroes {
     public static void main(String[] args) {
-        System.out.println(new FactorialTrailingZeroes().trailingZeroes(5));
+        System.out.println(new FactorialTrailingZeroes().trailingZeroes(1808548329));
     }
 
     public int trailingZeroes(int n) {
         int power = 0;
-        int k = 1;
+        long k = 1;
         while (k <= n) {
             k *= 5;
             power++;
