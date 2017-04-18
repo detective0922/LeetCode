0f5491f36c07ce332115b65ea0bd6b31045642bc
detective0922 <detective0922@hotmail.com>
2017-04-18 10:10:59 +0800

    MinimumDepthofBinaryTree

--- LeetCode/src/MinimumDepthofBinaryTree.java
+++ LeetCode/src/MinimumDepthofBinaryTree.java
@@ -8,6 +8,55 @@ The minimum depth is the number of nodes along the shortest path from the root n
 */
 public class MinimumDepthofBinaryTree {
     public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
 
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
+        int leftDepth = 1;
+        if (root.left != null) {
+            leftDepth = maxDepth(root.left, leftDepth);
+        }
+
+        int rightDepth = 1;
+        if (root.right != null) {
+            rightDepth = maxDepth(root.right, rightDepth);
+        }
+
+        if (leftDepth > rightDepth) {
+            return leftDepth;
+        } else {
+            return rightDepth;
+        }
+
+    }
+
+    public static int maxDepth(TreeNode node, int depth) {
+        if (node == null) {
+            return depth;
+        }
+
+        depth++;
+        if (node.left == null && node.right == null) {
+            return depth;
+        }
+
+        int leftDepth = 0;
+        if (node.left != null) {
+            leftDepth = maxDepth(node.left, depth);
+        }
+
+        int rightDepth = 0;
+        if (node.right != null) {
+            rightDepth = maxDepth(node.right, depth);
+        }
+
+        if (leftDepth > rightDepth) {
+            return leftDepth;
+        } else {
+            return rightDepth;
+        }
     }
 }
