8683bb907dc74479db64b5d896b55eb9e1630b7b
detective0922 <detective0922@hotmail.com>
2017-04-25 14:31:21 +0800

    ImplementStackusingQueues

--- LeetCode/src/ImplementStackusingQueues.java
+++ LeetCode/src/ImplementStackusingQueues.java
@@ -34,12 +34,12 @@ public class ImplementStackusingQueues {
 
     /** Removes the element on top of the stack and returns that element. */
     public int pop() {
-
+        return queue.poll();
     }
 
     /** Get the top element. */
     public int top() {
-
+        return queue.peek();
     }
 
     /** Returns whether the stack is empty. */
