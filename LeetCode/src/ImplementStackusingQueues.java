e4eab722843e4e6924b91d3bd19ac544c87449d6
detective0922 <detective0922@hotmail.com>
2017-04-25 14:19:02 +0800

    ImplementStackusingQueues

--- LeetCode/src/ImplementStackusingQueues.java
+++ LeetCode/src/ImplementStackusingQueues.java
@@ -15,5 +15,38 @@ Notes:
 
 */
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class ImplementStackusingQueues {
+
+    private Queue<Integer> queue;
+    public MyQueue() {
+        queue = new LinkedList<Integer>();
+    }
+
+    /** Initialize your data structure here. */
+    public MyStack() {
+
+    }
+
+    /** Push element x onto stack. */
+    public void push(int x) {
+
+    }
+
+    /** Removes the element on top of the stack and returns that element. */
+    public int pop() {
+
+    }
+
+    /** Get the top element. */
+    public int top() {
+
+    }
+
+    /** Returns whether the stack is empty. */
+    public boolean empty() {
+
+    }
 }
