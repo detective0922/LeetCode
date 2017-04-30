b1310a15f382ac77255d1fca1f9473c79e74f6cc
detective0922 <detective0922@hotmail.com>
2017-04-30 16:36:26 +0800

    MergeSortedArray

--- LeetCode/src/MergeSortedArray.java
+++ LeetCode/src/MergeSortedArray.java
@@ -15,19 +15,14 @@ public class MergeSortedArray {
         int k = m + n - 1;
         while (i >= 0 && j >= 0) {
             if (nums2[j] >= nums1[i]) {
-                nums1[k] = nums2[j];
-                j--;
+                nums1[k--] = nums2[j--];
             } else {
-                nums1[k] = nums1[i];
-                i--;
+                nums1[k--] = nums1[i--];
             }
-            k--;
         }
 
         while (i >= 0) {
-            nums1[k] = nums2[i];
-            k--;
-            i--;
+            nums1[k--] = nums2[i--];
         }
     }
 }
