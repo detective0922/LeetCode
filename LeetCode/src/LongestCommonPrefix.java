1536aadce24003c1dd21a290274deb34502cf50b
detective0922 <detective0922@hotmail.com>
2017-05-02 15:24:20 +0800

    LongestCommonPrefix

--- LeetCode/src/LongestCommonPrefix.java
+++ LeetCode/src/LongestCommonPrefix.java
@@ -18,10 +18,7 @@ public class LongestCommonPrefix {
         for (int i = 0; i < str.length(); i++) {
             char c = str.charAt(i);
             for (int j = 0; j < str.length(); j++) {
-                if (strs[j].length() < i + 1) {
-                    return commPerfix.toString();
-                }
-                if (strs[j].charAt(i)!=c){
+                if (strs[j].length() < i + 1 || strs[j].charAt(i) != c) {
                     return commPerfix.toString();
                 }
             }
