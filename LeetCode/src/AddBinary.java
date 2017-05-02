a633f6ee5e74e54e05ddfe2c53c77a2f35e1bdac
detective0922 <detective0922@hotmail.com>
2017-05-02 14:36:22 +0800

    AddBinary

--- LeetCode/src/AddBinary.java
+++ LeetCode/src/AddBinary.java
@@ -14,8 +14,13 @@ public class AddBinary {
     public String addBinary(String a, String b) {
         StringBuilder sum = new StringBuilder();
         int carry = 0;
-        for (int i = a.length(), j=b.length();i>=0||j>=0||carry==1;i--,j--){
-
+        int aNum = 0;
+        int bNum = 0;
+        for (int i = a.length() - 1, j = b.length() - 1; i >= 0 || j >= 0 || carry == 1; i--, j--) {
+            aNum = i < 0 ? 0 : a.charAt(i) - '0';
+            bNum = j < 0 ? 0 : b.charAt(j) - '0';
+            sum.append((aNum + bNum + carry) % 2);
+            carry = aNum + bNum + carry / 2;
         }
         return sum.reverse().toString();
 
