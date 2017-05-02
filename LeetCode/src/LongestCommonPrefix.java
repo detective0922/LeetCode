10cc5f0754d6769b2f9e7dca0586f2b7f4cfdf6b
detective0922 <detective0922@hotmail.com>
2017-05-02 14:56:34 +0800

    LongestCommonPrefix

--- LeetCode/src/LongestCommonPrefix.java
+++ LeetCode/src/LongestCommonPrefix.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /*
 14. Longest Common Prefix
 
@@ -6,6 +9,10 @@ Write a function to find the longest common prefix string amongst an array of st
 */
 public class LongestCommonPrefix {
     public String longestCommonPrefix(String[] strs) {
+        Map<Integer, Character> strMap = new HashMap<Integer, Character>();
+        for (int i = 0; i<strs.length;i++){
+
+        }
 
     }
 }
