3f013c6935f43644bfbf01277dcbd990907af588
detective0922 <detective0922@hotmail.com>
2017-05-04 16:50:37 +0800

    ReverseBits

--- LeetCode/src/ReverseBits.java
+++ LeetCode/src/ReverseBits.java
@@ -11,6 +11,10 @@ Follow up:
 
 */
 public class ReverseBits {
+    public static void main(String[] args) {
+        System.out.println(new ReverseBits().reverseBits(43261596));
+    }
+
     // you need treat n as an unsigned value
     public int reverseBits(int n) {
         int reverseInt = 0;
