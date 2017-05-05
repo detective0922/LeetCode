d38c9c8619439a11022441d41140a47f2966f99b
detective0922 <detective0922@hotmail.com>
2017-05-05 09:44:06 +0800

    RangeSumQueryImmutable

--- LeetCode/src/RangeSumQueryImmutable.java
+++ LeetCode/src/RangeSumQueryImmutable.java
@@ -20,4 +20,21 @@ Note:
 
 */
 public class RangeSumQueryImmutable {
+
 }
+class NumArray {
+
+    public NumArray(int[] nums) {
+
+    }
+
+    public int sumRange(int i, int j) {
+
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(i,j);
+ */
