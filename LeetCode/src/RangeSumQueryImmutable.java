5ec3a8c1a1e3aa98c73a084d88415ec54f051058
detective0922 <detective0922@hotmail.com>
2017-05-05 09:57:07 +0800

    RangeSumQueryImmutable

--- LeetCode/src/RangeSumQueryImmutable.java
+++ LeetCode/src/RangeSumQueryImmutable.java
@@ -20,6 +20,13 @@ Note:
 
 */
 public class RangeSumQueryImmutable {
+    public static void main(String[] args) {
+        int[] nums = {-2,0,3,-5,2,-1};
+        NumArray obj = new NumArray(nums);
+        System.out.println(obj.sumRange(0, 2));
+        System.out.println(obj.sumRange(2, 5));
+        System.out.println(obj.sumRange(0, 5));
+    }
 
 }
 class NumArray {
