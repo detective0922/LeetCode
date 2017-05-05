0033db46966052b6b7c3cbacaeb6bc43062ff02e
detective0922 <detective0922@hotmail.com>
2017-05-05 10:37:47 +0800

    MinStack

--- LeetCode/src/MinStack.java
+++ LeetCode/src/MinStack.java
@@ -20,5 +20,36 @@ minStack.top();      --> Returns 0.
 minStack.getMin();   --> Returns -2.
 
 */
+
 public class MinStack {
+
+    /** initialize your data structure here. */
+    public MinStack() {
+
+    }
+
+    public void push(int x) {
+
+    }
+
+    public void pop() {
+
+    }
+
+    public int top() {
+
+    }
+
+    public int getMin() {
+
+    }
 }
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
