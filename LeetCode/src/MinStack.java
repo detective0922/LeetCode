a046674a11f097ca72d95ff7ac06549a4a362240
detective0922 <detective0922@hotmail.com>
2017-05-08 10:33:09 +0800

    MinStack

--- LeetCode/src/MinStack.java
+++ LeetCode/src/MinStack.java
@@ -33,6 +33,23 @@ public class MinStack {
         min = Integer.MAX_VALUE;
     }
 
+    // a better solution
+    /*public void push(int x) {
+        // only push the old minimum value when the current
+        // minimum value changes after pushing the new value x
+        if(x <= min){
+            stack.push(min);
+            min=x;
+        }
+        stack.push(x);
+    }
+
+    public void pop() {
+        // if pop operation could result in the changing of the current minimum value,
+        // pop twice and change the current minimum value to the last minimum value.
+        if(stack.pop() == min) min=stack.pop();
+    }*/
+
     public void push(int x) {
         stack.push(x);
         if (x < min) {
