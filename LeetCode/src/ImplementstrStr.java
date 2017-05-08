b15932f003a9017c5bdec29aef326402fa0c77a6
detective0922 <detective0922@hotmail.com>
2017-05-08 11:27:57 +0800

    ImplementstrStr

--- LeetCode/src/ImplementstrStr.java
+++ LeetCode/src/ImplementstrStr.java
@@ -9,6 +9,12 @@ Returns the index of the first occurrence of needle in haystack, or -1 if needle
 */
 public class ImplementstrStr {
     public int strStr(String haystack, String needle) {
+        if (haystack == null || needle == null
+                || haystack.isEmpty() || needle.isEmpty()) {
+            return -1;
+        }
+
+
 
     }
 }
