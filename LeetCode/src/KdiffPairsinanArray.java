bc74cf7e102f61c17654f3a8bd51b645280d4d8c
detective0922 <detective0922@hotmail.com>
2017-05-09 14:52:36 +0800

    KdiffPairsinanArray

--- LeetCode/src/KdiffPairsinanArray.java
+++ LeetCode/src/KdiffPairsinanArray.java
@@ -44,32 +44,11 @@ public class KdiffPairsinanArray {
 
     public int findPairs(int[] nums, int k) {
         Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-        for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (Math.abs(nums[j] - nums[i]) == k) {
-                    boolean iKey = map.containsKey(nums[i]);
-                    boolean jKey = map.containsKey(nums[j]);
-                    if (iKey) {
-                        System.out.println(map.get(nums[i]));
-                    }
-                    if (jKey) {
-                        System.out.println(map.get(nums[j]));
-                    }
-                    if (((iKey) && map.get(nums[i]).equals(nums[j]))
-                            || ((jKey) && map.get(nums[j]).equals(nums[i]))) {
-                        continue;
-                    } else if (!iKey) {
-                        map.put(nums[i], nums[j]);
-                    } else if (!jKey) {
-                        map.put(nums[j], nums[i]);
-                    }
-                }
-            }
+        int count = 0;
+        for(int num : nums) {
+
         }
-        for (Map.Entry<Integer,Integer> entry : map.entrySet()) {
-            System.out.println(entry.getKey() + ", " + entry.getValue());
-        }
-        return map.size();
+        return count;
 
     }
 }
