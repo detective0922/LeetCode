80a3b1248e6569c514f436c4461c20a8325ec76c
detective0922 <detective0922@hotmail.com>
2017-05-10 14:40:57 +0800

    CountPrimes

--- LeetCode/src/CountPrimes.java
+++ LeetCode/src/CountPrimes.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 /*
@@ -11,19 +12,22 @@ Count the number of prime numbers less than a non-negative number, n.
 public class CountPrimes {
 
     public static void main(String[] args) {
-        System.out.println(new CountPrimes().countPrimes(72));
+        System.out.println(new CountPrimes().countPrimes( 499979));
     }
 
     public int countPrimes(int n) {
-        if (n == 0 && n == 1) {
-            return n;
+        if (n < 2) {
+            return 0;
         }
         List<Integer> primes = new ArrayList<Integer>();
+        HashSet<Integer> notPrimes = new HashSet<Integer>();
         for (int i = 2; i < n; i++) {
             boolean isPrime = true;
             for (int prime : primes) {
                 if (i % prime == 0) {
+                    notPrimes.add(i);
                     isPrime = false;
+                    break;
                 }
             }
             if (isPrime) {
