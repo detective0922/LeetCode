1f6052dfc48039e15803d74b79060112522b82bc
detective0922 <detective0922@hotmail.com>
2017-05-11 02:16:18 +0800

    Sqrt

--- LeetCode/src/Sqrt.java
+++ LeetCode/src/Sqrt.java
@@ -9,6 +9,11 @@ Compute and return the square root of x.
 public class Sqrt {
     public int mySqrt(int x) {
         int sqrt = 0;
+        int low = 0;
+        int high = x;
+        while (low <= high ) {
+            int mid = (high - low) / 2 + low;
+        }
 
         return sqrt;
 
