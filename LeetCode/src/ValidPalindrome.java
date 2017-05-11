6030a3ebf156fdb632e97ce8f0aec66b1fd643b5
detective0922 <detective0922@hotmail.com>
2017-05-11 08:34:28 +0800

    ValidPalindrome

--- LeetCode/src/ValidPalindrome.java
+++ LeetCode/src/ValidPalindrome.java
@@ -15,6 +15,31 @@ For the purpose of this problem, we define empty string as valid palindrome.
 */
 public class ValidPalindrome {
     public boolean isPalindrome(String s) {
+        if (s == null) {
+            return false;
+        }
+        if (s.isEmpty()) {
+            return true;
+        }
+        int i = 0;
+        int j = s.length() - 1;
+        while (i < j) {
+            char start = s.charAt(i);
+            char end = s.charAt(j);
+            boolean isStartValid = Character.isLetterOrDigit(start);
+            boolean isEndValid = Character.isLetterOrDigit(end);
+            if (Character.isLetterOrDigit(start) && Character.isLetterOrDigit(end)) {
+
+            }
+            if (!Character.isLetterOrDigit(start)) {
+                i++;
+            }
+            if (!Character.isLetterOrDigit(end)) {
+                j--;
+            }
+
+        }
+
 
     }
 }
