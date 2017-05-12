4978ec8c3e2e1d5b81bccae9f0802e5d68062cf3
detective0922 <detective0922@hotmail.com>
2017-05-12 16:42:22 +0800

    ReverseInteger

--- LeetCode/src/ReverseInteger.java
+++ LeetCode/src/ReverseInteger.java
@@ -8,10 +8,22 @@ Example2: x = -123, return -321
 
 */
 public class ReverseInteger {
+
+    public static void main(String[] args) {
+        System.out.println(new ReverseInteger().reverse(-2147483648));
+    }
+
     public int reverse(int x) {
-        int tmp = x >= 0? x: -x;
+        int tmp = x >= 0 ? x : -x;
 
-
-        return x>=0? tmp : -tmp;
+        String tmpStr = Long.toString(tmp);
+        StringBuilder tmpStrBuilder = new StringBuilder(tmpStr);
+        tmpStr = tmpStrBuilder.reverse().toString();
+        long tmpLong = Long.parseLong(tmpStr);
+        if (tmpLong > Integer.MAX_VALUE) {
+            return 0;
+        }
+        tmp = (int) tmpLong;
+        return x >= 0 ? tmp : -tmp;
     }
 }
