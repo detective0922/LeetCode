8791775a31205a9d8eec23390f6d0dfc49de6062
detective0922 <detective0922@hotmail.com>
2017-05-12 13:34:50 +0800

    FirstBadVersion

--- LeetCode/src/FirstBadVersion.java
+++ LeetCode/src/FirstBadVersion.java
@@ -19,6 +19,16 @@ Implement a function to find the first bad version. You should minimize the numb
       boolean isBadVersion(int version); */
 public class FirstBadVersion {
     public int firstBadVersion(int n) {
+        int low = 1;
+        int high = n;
+        while ( low < high) {
+            int mid = (high - low) / 2 + low;
+            if (isBadVersion(mid)) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
 
     }
 }
