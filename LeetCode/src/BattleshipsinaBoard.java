858c3dc18ffba241cb0522233bd74a73b3cf391d
detective0922 <detective0922@hotmail.com>
2017-05-15 11:24:51 +0800

    BattleshipsinaBoard

--- LeetCode/src/BattleshipsinaBoard.java
+++ LeetCode/src/BattleshipsinaBoard.java
@@ -35,10 +35,12 @@ public class BattleshipsinaBoard {
         }
         boolean[][] isCounted = new boolean[board.length][board[0].length];
 
-        for (int i = 0; i< board.length; i++) {
-            for (int j = 0; j< board[0].length; j++) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
                 if (!isCounted[i][j]) {
+                    if (board[i][j] == 'X') {
 
+                    }
                 }
             }
         }
