17c54799e91cf38d172ba36c4c6d2cfdd58f96c0
detective0922 <detective0922@hotmail.com>
2017-05-15 10:43:05 +0800

    ComplexNumberMultiplication

--- LeetCode/src/ComplexNumberMultiplication.java
+++ LeetCode/src/ComplexNumberMultiplication.java
@@ -22,8 +22,12 @@ Explanation: (1 - i) * (1 - i) = 1 + i2 - 2 * i = -2i, and you need convert it t
 
 Note:
 1.The input strings will not have extra blank.
-2.The input strings will be given in the form of a+bi, where the integer a and b will both belong to the range of [-100, 100]. And the output should be also in this form.
+2.The input strings will be given in the form of a+bi, where the integer a and b will both belong to the range of [-100, 100].
+And the output should be also in this form.
 
 */
 public class ComplexNumberMultiplication {
+    public String complexNumberMultiply(String a, String b) {
+
+    }
 }
