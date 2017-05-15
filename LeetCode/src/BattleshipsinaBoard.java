3a9894f113509839db68fcf7f0a9cd40925567f4
detective0922 <detective0922@hotmail.com>
2017-05-15 13:50:13 +0800

    BattleshipsinaBoard

--- LeetCode/src/BattleshipsinaBoard.java
+++ LeetCode/src/BattleshipsinaBoard.java
@@ -60,5 +60,17 @@ public class BattleshipsinaBoard {
             }
         }
         return count;
+
+        //a simple solution:
+        /*int count = 0;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == 'X' && (i == 0 || board[i-1][j] == '.') && (j == 0 || board[i][j-1] == '.')) {
+                    count++;
+                }
+            }
+        }
+        return count;*/
+
     }
 }
