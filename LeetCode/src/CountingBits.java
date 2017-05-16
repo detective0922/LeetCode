4515c8459f84c9cd5d01d30f6694487d2e3c567d
detective0922 <detective0922@hotmail.com>
2017-05-16 14:32:02 +0800

    CountingBits

--- LeetCode/src/CountingBits.java
+++ LeetCode/src/CountingBits.java
@@ -33,5 +33,19 @@ public class CountingBits {
             countBits[i] = count;
         }
         return countBits;
+        // some better solution
+        /*int[] answer = new int[num+1];
+        int offset = 1;
+        for(int i = 1; i < answer.length; i++){
+            if(offset * 2 == i) offset *= 2;
+            answer[i] = 1 + answer[i - offset];
+        }
+        return answer;*/
+
+        /*int[] f = new int[num + 1];
+        for (int i=1; i<=num; i++) f[i] = f[i >> 1] + (i & 1);
+        return f;*/
+
+
     }
 }
