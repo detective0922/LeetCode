c3b867a153ab24c4771e1fba5f9a2fa1e5b020ab
detective0922 <detective0922@hotmail.com>
2017-05-16 14:36:04 +0800

    FindBottomLeftTreeValue

--- LeetCode/src/FindBottomLeftTreeValue.java
+++ LeetCode/src/FindBottomLeftTreeValue.java
@@ -1,3 +1,8 @@
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+
 /*
 
 513. Find Bottom Left Tree Value
@@ -34,4 +39,19 @@ Note: You may assume the tree (i.e., the given root node) is not NULL.
 
 */
 public class FindBottomLeftTreeValue {
+    private int maxDepth = 0;
+    private Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+    public int findBottomLeftValue(TreeNode root) {
+        int depth = 0;
+        recordLeft(root, depth);
+        return map.get(maxDepth);
+    }
+
+    public void recordLeft(TreeNode node, int depth) {
+        if (depth > maxDepth) {
+            maxDepth = depth;
+            map.put(maxDepth, node.val);
+        }
+
+    }
 }
