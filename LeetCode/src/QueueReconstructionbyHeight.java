3c9434318712a1acd3cea160b2316f484dde22a0
detective0922 <detective0922@hotmail.com>
2017-05-18 17:14:12 +0800

    QueueReconstructionbyHeight

--- LeetCode/src/QueueReconstructionbyHeight.java
+++ LeetCode/src/QueueReconstructionbyHeight.java
@@ -35,7 +35,7 @@ public class QueueReconstructionbyHeight {
         Arrays.sort(people, new Comparator<int[]>() {
             @Override
             public int compare(int[] o1, int[] o2) {
-                return o1[0]!=o2[0]? o1[0] - o2[0]:o1[1] - o2[1];
+                return o1[0]!=o2[0]? o2[0] - o1[0]:o1[1] - o2[1];
             }
         });
 
