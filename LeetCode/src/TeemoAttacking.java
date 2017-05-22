747cb6517367e3953717d1e8dfc5a06dbb6a928a
detective0922 <detective0922@hotmail.com>
2017-05-22 15:46:23 +0800

    TeemoAttacking

--- LeetCode/src/TeemoAttacking.java
+++ LeetCode/src/TeemoAttacking.java
@@ -40,7 +40,7 @@ Note:
 public class TeemoAttacking {
 
     public static void main(String[] args) {
-        int[] timeSeries = {1, 2};
+        int[] timeSeries = {1, 4};
         System.out.println(new TeemoAttacking().findPoisonedDuration(timeSeries, 2));
     }
 
@@ -48,11 +48,8 @@ public class TeemoAttacking {
         if (timeSeries.length == 0) {
             return 0;
         }
-        if (timeSeries.length == 1) {
-            return duration;
-        }
 
-        int totalTime = 0;
+        int totalTime = duration;
         for (int i = 1; i < timeSeries.length; i++) {
             int singleTime = timeSeries[i] - timeSeries[i - 1];
             if (singleTime > duration) {
