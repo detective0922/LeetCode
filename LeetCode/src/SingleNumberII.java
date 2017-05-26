835711bbe9da09990d6dec683ff7eb8f530824e2
detective0922 <detective0922@hotmail.com>
2017-05-26 18:13:10 +0800

    SingleNumberII

--- LeetCode/src/SingleNumberII.java
+++ LeetCode/src/SingleNumberII.java
@@ -1,3 +1,9 @@
+/*
+137. Single Number II
 
+Given an array of integers, every element appears three times except for one, which appears exactly once. Find that single one.
+
+
+*/
 public class SingleNumberII {
 }
