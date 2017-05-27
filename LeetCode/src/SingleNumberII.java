6df1dd0707a22522dd060b9f822df525e2120eea
detective0922 <detective0922@hotmail.com>
2017-05-27 17:21:47 +0800

    SingleNumberII

--- LeetCode/src/SingleNumberII.java
+++ LeetCode/src/SingleNumberII.java
@@ -6,4 +6,12 @@ Given an array of integers, every element appears three times except for one, wh
 
 */
 public class SingleNumberII {
+    public int singleNumber(int[] A) {
+        int ones = 0, twos = 0;
+        for(int i = 0; i < A.length; i++){
+            ones = (ones ^ A[i]) & ~twos;
+            twos = (twos ^ A[i]) & ~ones;
+        }
+        return ones;
+    }
 }
