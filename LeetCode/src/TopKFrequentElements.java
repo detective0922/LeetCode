6568c1212d86e2fa667b0773bf04a5190a576537
detective0922 <detective0922@hotmail.com>
2017-06-01 17:17:13 +0800

    TopKFrequentElements

--- LeetCode/src/TopKFrequentElements.java
+++ LeetCode/src/TopKFrequentElements.java
@@ -35,7 +35,7 @@ public class TopKFrequentElements {
         PriorityQueue<Map.Entry<Integer, Integer>> queue = new PriorityQueue<Map.Entry<Integer, Integer>>(freqMap.size(), new Comparator<Map.Entry<Integer, Integer>>() {
             @Override
             public int compare(Map.Entry<Integer, Integer> o1, Map.Entry<Integer, Integer> o2) {
-                return o1.getValue() - o2.getValue();
+                return o2.getValue() - o1.getValue();
             }
         });
         queue.addAll(freqMap.entrySet());
