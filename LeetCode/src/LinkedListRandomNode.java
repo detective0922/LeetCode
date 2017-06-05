d96db6c3f92b056bb7d4435b332d5855bad07a60
detective0922 <detective0922@hotmail.com>
2017-06-05 11:00:07 +0800

    LinkedListRandomNode

--- LeetCode/src/LinkedListRandomNode.java
+++ LeetCode/src/LinkedListRandomNode.java
@@ -22,6 +22,7 @@ solution.getRandom();
 public class LinkedListRandomNode {
 
     private ListNode head;
+    private Random random = new Random();
     /** @param head The linked list's head.
     Note that the head is guaranteed to be not null, so it contains at least one node. */
     public Solution(ListNode head) {
@@ -30,7 +31,6 @@ public class LinkedListRandomNode {
 
     /** Returns a random node's value. */
     public int getRandom() {
-        Random random = new Random();
         ListNode currNode = head;
         int randomInt = head.val;
         for (int i = 0; currNode != null; currNode = currNode.next, i++) {
