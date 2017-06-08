877825e1cee654c18c43b930af8b0ef8a88e9a1c
detective0922 <detective0922@hotmail.com>
2017-06-08 13:48:13 +0800

    IntegertoRoman

--- LeetCode/src/IntegertoRoman.java
+++ LeetCode/src/IntegertoRoman.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /*
 12. Integer to Roman
 
@@ -9,6 +12,27 @@ Input is guaranteed to be within the range from 1 to 3999.
 */
 public class IntegertoRoman {
     public String intToRoman(int num) {
+        Map<Integer, Character> map = new HashMap<Integer, Character>();
+        StringBuilder roman = new StringBuilder();
+        int unit = 0;
+        while (num > 0) {
+            if (num >= 1000) {
+                unit = 1000;
+            } else if (num >= 500) {
+                unit = 500;
+            } else if (num >= 100) {
+                unit = 100;
+            } else if (num >= 50) {
+                unit = 50;
+            } else if (num >= 10) {
+                unit = 10;
+            } else if (num >= 5) {
+                unit = 5;
+            } else if (num >= 1) {
+                unit = 1;
+            }
+
+        }
 
     }
 }
