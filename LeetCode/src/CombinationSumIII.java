c1e5bb69362e6a8b5d4d13ecbb4a7ec18a7fc5ec
detective0922 <detective0922@hotmail.com>
2017-06-08 18:11:49 +0800

    CombinationSumIII

--- LeetCode/src/CombinationSumIII.java
+++ LeetCode/src/CombinationSumIII.java
@@ -34,5 +34,8 @@ public class CombinationSumIII {
     public List<List<Integer>> combinationSum3(int k, int n) {
         List<List<Integer>> sums = new ArrayList<List<Integer>>();
 
+
+        return sums;
+
     }
 }
