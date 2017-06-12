cadf977eac319a4d66d791bdf3690e40c99a3a31
detective0922 <detective0922@hotmail.com>
2017-06-12 14:54:13 +0800

    GenerateParentheses

--- LeetCode/src/GenerateParentheses.java
+++ LeetCode/src/GenerateParentheses.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 /*
@@ -18,6 +19,19 @@ For example, given n = 3, a solution set is:
 */
 public class GenerateParentheses {
     public List<String> generateParenthesis(int n) {
+        List<String> parentheses = new ArrayList<String>();
+        int left = n;
+        int right = n;
+        generateParentheses(parentheses, left, right, n);
+        return parentheses;
+    }
+
+    private void generateParentheses(List<String> parentheses, int left, int right, int n) {
+        if (left > n || right > n) {
+            return;
+        }
 
     }
+
+
 }
