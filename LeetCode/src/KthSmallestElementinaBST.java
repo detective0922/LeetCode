ac26448ecf2cda8771da93e10c0e8db3661ff1b1
detective0922 <detective0922@hotmail.com>
2017-06-20 16:25:28 +0800

    KthSmallestElementinaBST

--- LeetCode/src/KthSmallestElementinaBST.java
+++ LeetCode/src/KthSmallestElementinaBST.java
@@ -1,7 +1,5 @@
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 
 /*
 230. Kth Smallest Element in a BST
@@ -22,16 +20,14 @@ public class KthSmallestElementinaBST {
     public static void main(String[] args) {
         TreeNode root = new TreeNode(1);
         TreeNode two = new TreeNode(2);
-        TreeNode Null = new TreeNode(3);
 
         root.right = two;
-        root.left = Null;
-        System.out.println(new KthSmallestElementinaBST().kthSmallest(root));
+        root.left = null;
+        System.out.println(new KthSmallestElementinaBST().kthSmallest(root, 2));
     }
 
     public int kthSmallest(TreeNode root, int k) {
         List<TreeNode> dfsList = new ArrayList<TreeNode>();
-        dfsList.add(root);
         dfs(root, dfsList);
         return dfsList.get(k - 1).val;
     }
