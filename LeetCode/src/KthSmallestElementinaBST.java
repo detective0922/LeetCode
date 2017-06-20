a95234af2fbd5cdc0a91a66f4abb5f11e258843e
detective0922 <detective0922@hotmail.com>
2017-06-20 15:40:13 +0800

    KthSmallestElementinaBST

--- LeetCode/src/KthSmallestElementinaBST.java
+++ LeetCode/src/KthSmallestElementinaBST.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 /*
 230. Kth Smallest Element in a BST
 
@@ -14,6 +17,51 @@ Follow up:
 */
 public class KthSmallestElementinaBST {
     public int kthSmallest(TreeNode root, int k) {
+        int depth = 1;
+
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        int leftCount = 1;
+        queue.add(root.left);
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (node != null) {
+                leftCount++;
+                queue.add(node.left);
+                queue.add(node.right);
+            }
+        }
+        if (leftCount > k) {
+
+        }
 
     }
+
+    public int maxDepth(TreeNode node, int depth) {
+        if (node == null) {
+            return depth;
+        }
+
+        depth++;
+        if (node.left == null && node.right == null) {
+            return depth;
+        }
+
+        int leftDepth = 0;
+        if (node.left != null) {
+            leftDepth = maxDepth(node.left, depth);
+        }
+
+        int rightDepth = 0;
+        if (node.right != null) {
+            rightDepth = maxDepth(node.right, depth);
+        }
+
+        if (leftDepth > rightDepth) {
+            return leftDepth;
+        } else {
+            return rightDepth;
+        }
+    }
+
+
 }
