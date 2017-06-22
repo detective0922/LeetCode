f866bc9526885b7dc363f54523688ed7ea6b5f45
detective0922 <detective0922@hotmail.com>
2017-06-22 16:59:13 +0800

    RandomPickIndex

--- LeetCode/src/RandomPickIndex.java
+++ LeetCode/src/RandomPickIndex.java
@@ -21,5 +21,18 @@ solution.pick(1);
 
 */
 
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(nums);
+ * int param_1 = obj.pick(target);
+ */
 public class RandomPickIndex {
+    public Solution(int[] nums) {
+
+    }
+
+    public int pick(int target) {
+
+    }
 }
