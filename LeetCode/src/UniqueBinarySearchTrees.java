b811450e673e63ab15ed60bf3b2453669d94e16b
detective0922 <detective0922@hotmail.com>
2017-07-12 19:13:24 +0800

    UniqueBinarySearchTrees

--- LeetCode/src/UniqueBinarySearchTrees.java
+++ LeetCode/src/UniqueBinarySearchTrees.java
@@ -17,7 +17,6 @@ public class UniqueBinarySearchTrees {
         int[] dp = new int[n+1];
         dp[0] = 0;
         dp[1] = 1;
-        dp[2] = 2;
         for (int i = 3; i <= n; i++) {
 
         }
