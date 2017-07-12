010dfca906063e751ebb2ed9ed94f86c0ceab917
detective0922 <detective0922@hotmail.com>
2017-07-12 13:56:36 +0800

    UniquePaths

--- LeetCode/src/UniquePaths.java
+++ LeetCode/src/UniquePaths.java
@@ -18,11 +18,29 @@ Note: m and n will be at most 100.
 */
 
 public class UniquePaths {
+
+    public static void main(String[] args) {
+        System.out.println(new UniquePaths().uniquePaths(4,6));
+    }
+
     public int uniquePaths(int m, int n) {
-        int paths = 0;
-        int currX = 0;
-        int currY = 0;
-        findPaths(currX, currY, m, n, paths);
-        return paths;
+        if (m == 0 || n == 0) {
+            return 0;
+        }
+        if (m == 1 || n == 1) {
+            return 1;
+        }
+
+        int[] dp = new int[n];
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[j] += dp[j - 1];
+            }
+        }
+        return dp[n - 1];
+
     }
 }
