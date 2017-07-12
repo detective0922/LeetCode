43c366410f907aed47fc23769fec2b863d7095d1
detective0922 <detective0922@hotmail.com>
2017-07-12 11:04:02 +0800

    GrayCode

--- LeetCode/src/GrayCode.java
+++ LeetCode/src/GrayCode.java
@@ -29,8 +29,13 @@ For now, the judge is able to judge based on one instance of gray code sequence.
 public class GrayCode {
     public List<Integer> grayCode(int n) {
         int size = (int)Math.pow(2, n);
+
         List<Integer> retList = new ArrayList<Integer>();
+        int code = 0;
         while (retList.size() < size) {
+            retList.add(code);
+            int mask = 1;
+            while ()
 
         }
 
