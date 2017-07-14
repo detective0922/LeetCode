e12289ab89feca2111e5eb3b21ce5833c0c76798
detective0922 <detective0922@hotmail.com>
2017-07-14 21:16:24 +0800

    EvaluateDivision

--- LeetCode/src/EvaluateDivision.java
+++ LeetCode/src/EvaluateDivision.java
@@ -24,6 +24,7 @@ The input is always valid. You may assume that evaluating the queries will resul
 */
 public class EvaluateDivision {
     public double[] calcEquation(String[][] equations, double[] values, String[][] queries) {
+        double[] answers = new double[queries.length];
 
     }
 }
