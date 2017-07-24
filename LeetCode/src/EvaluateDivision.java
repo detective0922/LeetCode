2c4daaf487ba2c8b8efe1db32f268b98fd8f7dc3
detective0922 <detective0922@hotmail.com>
2017-07-25 00:19:18 +0800

    EvaluateDivision

--- LeetCode/src/EvaluateDivision.java
+++ LeetCode/src/EvaluateDivision.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /*
@@ -54,7 +55,24 @@ public class EvaluateDivision {
 
     private double dfs(String v, String w, Map<String, ArrayList<String>> edges, Map<String, ArrayList<Double>> weights, ArrayList<String> visited, double currentWeight) {
         if (visited.contains(v)) {
+            return -1.0;
+        }
+        if (!edges.containsKey(v)) {
+            return -1.0;
+        }
+        if(v.equals(w)) {
+            return currentWeight;
+        }
 
+        visited.add(v);
+        List<String> subEdges = edges.get(v);
+        List<Double> subWeights = weights.get(v);
+        double tmpWeight;
+        for (int i = 0; i < subEdges.size(); i++) {
+            tmpWeight = dfs(subEdges.get(i), w, edges, weights, visited, currentWeight * subWeights.get(i));
+            if (tmpWeight != -1.0) {
+                break;
+            }
         }
     }
 }
