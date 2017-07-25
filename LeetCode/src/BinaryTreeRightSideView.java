4506b65e41a266a014b087759f6e4e4ae52bf9b7
detective0922 <detective0922@hotmail.com>
2017-07-26 00:27:24 +0800

    BinaryTreeRightSideView

--- LeetCode/src/BinaryTreeRightSideView.java
+++ LeetCode/src/BinaryTreeRightSideView.java
@@ -47,7 +47,19 @@ public class BinaryTreeRightSideView {
         Queue<TreeNode> queue = new LinkedList<TreeNode>();
         queue.offer(root);
         while (!queue.isEmpty()) {
-
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (i == size - 1) {
+                    vals.add(node.val);
+                }
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
         }
 
     }
