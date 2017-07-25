05af9dfef1799197c4f2d519a25c10f693ac779f
detective0922 <detective0922@hotmail.com>
2017-07-26 00:04:08 +0800

    BinaryTreeRightSideView

--- LeetCode/src/BinaryTreeRightSideView.java
+++ LeetCode/src/BinaryTreeRightSideView.java
@@ -19,6 +19,18 @@ You should return [1, 3, 4].
 
 */
 public class BinaryTreeRightSideView {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        TreeNode two = new TreeNode(9);
+        TreeNode three = new TreeNode(20);
+        TreeNode four = new TreeNode(15);
+        TreeNode five = new TreeNode(7);
+        root.left = two;
+        root.right = three;
+        three.left = four;
+        three.right = five;
+        System.out.println(new BinaryTreeRightSideView().rightSideView(root));
+    }
     public List<Integer> rightSideView(TreeNode root) {
         Map<Integer, Integer> nodeMap = new HashMap<Integer, Integer>();
         int depth = 0;
