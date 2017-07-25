628aaea6e35011d33806110de2fdf250b59598b7
detective0922 <detective0922@hotmail.com>
2017-07-25 20:18:04 +0800

    BestTimetoBuyandSellStockwithCooldown

--- LeetCode/src/BestTimetoBuyandSellStockwithCooldown.java
+++ LeetCode/src/BestTimetoBuyandSellStockwithCooldown.java
@@ -20,9 +20,12 @@ public class BestTimetoBuyandSellStockwithCooldown {
     public int maxProfit(int[] prices) {
         //buy[i] = Math.max(buy[i - 1], sell[i - 2] - prices[i]);
         //sell[i] = Math.max(sell[i - 1], buy[i - 1] + prices[i]);
-        if (prices == null|| prices.length < 2) {
+        if (prices == null || prices.length < 2) {
             return 0;
         }
+        int[] buy = new int[prices.length];
+        int[] sell = new int[prices.length];
+
 
 
 
