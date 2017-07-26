bde7c6a24478fd9135bb3b315beb7f9439add3d3
detective0922 <detective0922@hotmail.com>
2017-07-26 19:52:33 +0800

    FindMinimuminRotatedSortedArray

--- LeetCode/src/FindMinimuminRotatedSortedArray.java
+++ LeetCode/src/FindMinimuminRotatedSortedArray.java
@@ -12,4 +12,12 @@ You may assume no duplicate exists in the array.
 
 */
 public class FindMinimuminRotatedSortedArray {
+    public int findMin(int[] nums) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+        }
+
+    }
 }
