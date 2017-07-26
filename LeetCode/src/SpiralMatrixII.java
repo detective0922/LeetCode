9323e89965f6b0d4cf8e87437af80b27494be9e2
detective0922 <detective0922@hotmail.com>
2017-07-26 23:13:14 +0800

    SpiralMatrixII

--- LeetCode/src/SpiralMatrixII.java
+++ LeetCode/src/SpiralMatrixII.java
@@ -13,5 +13,10 @@ You should return the following matrix: [
 
 */
 public class SpiralMatrixII {
+    public int[][] generateMatrix(int n) {
+        int[][] matrix = new int[n][n];
+
+        return matrix;
+    }
 
 }
