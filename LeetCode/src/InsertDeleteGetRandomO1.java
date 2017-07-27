ea6b64585628b1037c0ceb987ba18f84a3ad86c5
detective0922 <detective0922@hotmail.com>
2017-07-28 00:05:08 +0800

    InsertDeleteGetRandomO1

--- LeetCode/src/InsertDeleteGetRandomO1.java
+++ LeetCode/src/InsertDeleteGetRandomO1.java
@@ -35,5 +35,44 @@ randomSet.insert(2);
 randomSet.getRandom();
 
 */
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
+
 public class InsertDeleteGetRandomO1 {
+
+}
+
+public class RandomizedSet {
+
+    ArrayList<Integer> nums;
+    HashMap<Integer, Integer> positions;
+    /** Initialize your data structure here. */
+    public RandomizedSet() {
+        nums = new ArrayList<Integer>();
+        positions = new HashMap<Integer, Integer>();
+    }
+
+    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    public boolean insert(int val) {
+
+    }
+
+    /** Removes a value from the set. Returns true if the set contained the specified element. */
+    public boolean remove(int val) {
+
+    }
+
+    /** Get a random element from the set. */
+    public int getRandom() {
+
+    }
 }
