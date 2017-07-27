83a7b6b004fbfc700bb5a6e7d4f37c293cd0804b
detective0922 <detective0922@hotmail.com>
2017-07-27 19:39:43 +0800

    BinaryTreeLevelOrderTraversal

--- LeetCode/src/BinaryTreeLevelOrderTraversal.java
+++ LeetCode/src/BinaryTreeLevelOrderTraversal.java
@@ -40,15 +40,21 @@ public class BinaryTreeLevelOrderTraversal {
         if (root == null) {
             return;
         }
-        Queue<Integer> queue = new LinkedList<Integer>();
-        queue.offer(root.val);
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
         while (!queue.isEmpty()) {
             int size = queue.size();
+            List<Integer> level = new ArrayList<Integer>();
             for (int i = 0; i < size; i++) {
                 TreeNode node = queue.poll();
-
+                level.add(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
             }
         }
-
     }
 }
