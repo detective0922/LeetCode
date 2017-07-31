42cb2bb7472053f7481828c63cc7893d32f657b8
detective0922 <detective0922@hotmail.com>
2017-08-01 01:17:04 +0800

    RotateImage

--- LeetCode/src/RotateImage.java
+++ LeetCode/src/RotateImage.java
@@ -11,10 +11,14 @@ Follow up:
 
 */
 public class RotateImage {
+    public static void main(String[] args) {
+        new RotateImage().rotate(new int[][]{{1,2},{3,4}});
+    }
+
     public void rotate(int[][] matrix) {
         int n = matrix.length;
         for (int i = 0; i< n / 2; i++) {
-            for (int j = i; i < n - 2*i; j++ ) {
+            for (int j = i; j < n - i - 1; j++ ) {
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[n - j -1][i];
                 matrix[n - j -1][i] = matrix[n - i -1][n - j -1];
