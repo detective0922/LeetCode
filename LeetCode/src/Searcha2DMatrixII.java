8cb2704e26a49386523ebb990c302ca3d6862454
detective0922 <detective0922@hotmail.com>
2017-07-31 23:30:17 +0800

    Searcha2DMatrixII

--- LeetCode/src/Searcha2DMatrixII.java
+++ LeetCode/src/Searcha2DMatrixII.java
@@ -25,6 +25,9 @@ Given target = 20, return false.
 */
 public class Searcha2DMatrixII {
     public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
         int row = 0;
         int col = matrix[0].length - 1;
         while (row < matrix.length && col >= 0) {
