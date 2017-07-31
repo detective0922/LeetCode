4cb902c9946aa37cc426a41c8fe4bc1a2c7e2dae
detective0922 <detective0922@hotmail.com>
2017-08-01 01:09:09 +0800

    RotateImage

--- LeetCode/src/RotateImage.java
+++ LeetCode/src/RotateImage.java
@@ -16,10 +16,10 @@ public class RotateImage {
         for (int i = 0; i< n / 2; i++) {
             for (int j = i; i < n - 2*i; j++ ) {
                 int temp = matrix[i][j];
-                matrix[i][j] = matrix[j][n - i -1];
-                matrix[j][n - i -1] = matrix[n - i -1][n - j -1];
-                matrix[n - i -1][n - j -1] = matrix[n - j -1][n];
-                matrix[n - j -1][n] = temp;
+                matrix[i][j] = matrix[n - j -1][i];
+                matrix[n - j -1][i] = matrix[n - i -1][n - j -1];
+                matrix[n - i -1][n - j -1] = matrix[j][n - i -1];
+                matrix[j][n - i -1] = temp;
             }
         }
     }
