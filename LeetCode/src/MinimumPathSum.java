feea46a9cf554fc6f0fd4976e8942c9c00fb6eb3
detective0922 <detective0922@hotmail.com>
2017-08-02 01:57:10 +0800

    MinimumPathSum

--- LeetCode/src/MinimumPathSum.java
+++ LeetCode/src/MinimumPathSum.java
@@ -8,6 +8,9 @@ Note: You can only move either down or right at any point in time.
 
 */
 public class MinimumPathSum {
+    public static void main(String[] args) {
+        new MinimumPathSum().minPathSum(new int[][]{{1,2,5},{3,2,1}});
+    }
     public int minPathSum(int[][] grid) {
         if (grid == null || grid.length == 0 || grid[0].length == 0) {
             return 0;
@@ -18,10 +21,10 @@ public class MinimumPathSum {
         for (int i = 1; i < m; i++) {
             currCol[i] = currCol[i-1] + grid[0][i];
         }
-        for (int i = 1; i < n; i++) {
-            currCol[0] += grid[i][0];
-            for (int j = 1; j < m; j++) {
-                currCol[j] = Math.min(currCol[j-1], currCol[i]) + grid[i][j];
+        for (int j = 1; j < n; j++) {
+            currCol[0] += grid[0][j];
+            for (int i = 1; i < m; i++) {
+                currCol[i] = Math.min(currCol[i-1], currCol[i]) + grid[i][j];
             }
         }
         return currCol[m-1];
