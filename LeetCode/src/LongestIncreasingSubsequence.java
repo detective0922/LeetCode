90bb0e29e6873b29dc86c0e6a47b7a58cc6993dc
detective0922 <detective0922@hotmail.com>
2017-08-04 02:58:41 +0800

    LongestIncreasingSubsequence

--- LeetCode/src/LongestIncreasingSubsequence.java
+++ LeetCode/src/LongestIncreasingSubsequence.java
@@ -47,9 +47,6 @@ public class LongestIncreasingSubsequence {
             if (nums[i] > tails[i - 1]) {
                 tails[i - 1] = nums[i];
             }
-            if (nums[i] < tails[0]) {
-                tails[0] = nums[i];
-            }
             int low = 0;
             int high = i - 1;
             while (low <= high) {
