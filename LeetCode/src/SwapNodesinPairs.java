3e522809d216f2ed08ff93bd7e12e3a09ef311ae
detective0922 <detective0922@hotmail.com>
2017-08-05 00:37:22 +0800

    SwapNodesinPairs

--- LeetCode/src/SwapNodesinPairs.java
+++ LeetCode/src/SwapNodesinPairs.java
@@ -10,6 +10,21 @@ Your algorithm should use only constant space. You may not modify the values in 
 
 */
 public class SwapNodesinPairs {
+    public static void main(String[] args) {
+        ListNode root = new ListNode(1);
+        ListNode two = new ListNode(2);
+        ListNode three = new ListNode(3);
+        ListNode four = new ListNode(4);
+        ListNode five = new ListNode(5);
+        ListNode six = new ListNode(6);
+        ListNode seven = new ListNode(6);
+        root.next = two;
+        two.next = three;
+        three.next = four;
+        four.next = five;
+        new SwapNodesinPairs().swapPairs(root);
+    }
+
     public ListNode swapPairs(ListNode head) {
         if (head == null || head.next == null) {
             return head;
@@ -18,7 +33,7 @@ public class SwapNodesinPairs {
         newHead.next = head;
         ListNode currNode = newHead;
 
-        while (currNode != null && currNode.next !=null) {
+        while (currNode != null && currNode.next != null && currNode.next.next != null) {
             ListNode tmpNode = currNode.next;
             currNode.next = currNode.next.next;
             tmpNode.next = tmpNode.next.next;
