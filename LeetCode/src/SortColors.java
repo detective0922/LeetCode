4d2ce92639418346a14a8b3d4bf4b315d107bf67
detective0922 <detective0922@hotmail.com>
2017-08-05 02:28:56 +0800

    sortColors

--- LeetCode/src/SortColors.java
+++ LeetCode/src/SortColors.java
@@ -30,7 +30,7 @@ public class SortColors {
         if (nums == null || nums.length == 0) {
             return;
         }
-        int i = 0;
+        /*int i = 0;
         int j = 0;
         int color = 0;
         while (i < nums.length) {
@@ -50,7 +50,25 @@ public class SortColors {
                     j = i;
                 }
             }
+        }*/
+        int low = 0;
+        int high = nums.length-1;
+        for(int i = low; i<=high;) {
+            if(nums[i]==0) {
+                int temp = nums[i];
+                nums[i] = nums[low];
+                nums[low]=temp;
+                i++;low++;
+            }else if(nums[i]==2) {
+                int temp = nums[i];
+                nums[i] = nums[high];
+                nums[high]=temp;
+                high--;
+            }else {
+                i++;
+            }
         }
+
     }
 
     private void swap(int[] nums, int i, int j) {
