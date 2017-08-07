cb64eb18f140afdbc834a2ab2842824f568b96e1
detective0922 <detective0922@hotmail.com>
2017-08-08 02:17:57 +0800

    FindPeakElement

--- LeetCode/src/FindPeakElement.java
+++ LeetCode/src/FindPeakElement.java
@@ -24,10 +24,15 @@ public class FindPeakElement {
         int high = nums.length - 1;
         while (low < high) {
             int mid = low + (high - low) / 2;
-            if (nums[mid] < nums[mid - 1] && nums[mid] > nums[mid + 1]) {
+            if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) {
                 return mid;
+            } else if (nums[mid] < nums[mid - 1] && nums[mid] > nums[mid + 1]) {
+                high = mid;
+            } else if (nums[mid] > nums[mid - 1] && nums[mid] < nums[mid + 1]) {
+                low = mid;
+            } else {
+                low = mid;
             }
-
         }
         return low;
     }
