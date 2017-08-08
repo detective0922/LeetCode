13d68d06f53e7dbfab88b6fad1c9d772e5db1c96
detective0922 <detective0922@hotmail.com>
2017-08-08 20:09:48 +0800

    PopulatingNextRightPointersinEachNode

--- LeetCode/src/PopulatingNextRightPointersinEachNode.java
+++ LeetCode/src/PopulatingNextRightPointersinEachNode.java
@@ -42,6 +42,9 @@ After calling your function, the tree should look like:
 
 */
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Definition for binary tree with next pointer.
  * public class TreeLinkNode {
@@ -53,7 +56,20 @@ After calling your function, the tree should look like:
 
 public class PopulatingNextRightPointersinEachNode {
     public void connect(TreeLinkNode root) {
-
+        if (root == null) {
+            return;
+        }
+        Queue<TreeLinkNode> queue = new LinkedList<TreeLinkNode>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            TreeLinkNode node = null;
+            for (int i = 0; i < size; i++) {
+                node = queue.poll();
+                node.next = queue.peek();
+            }
+            node.next = null;
+        }
     }
 }
 
