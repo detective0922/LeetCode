0f6c04460a4ef6a039df23680c8b89ab754e77cd
detective0922 <detective0922@hotmail.com>
2017-08-09 01:53:39 +0800

    GameofLife

--- LeetCode/src/GameofLife.java
+++ LeetCode/src/GameofLife.java
@@ -29,6 +29,9 @@ which would cause problems when the active area encroaches the border of the arr
 public class GameofLife {
     public void gameOfLife(int[][] board) {
         for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                
+            }
             
         }
 
