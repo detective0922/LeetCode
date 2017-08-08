a8d4847eb13170cb62dc389a47f7c1c296d1f3e7
detective0922 <detective0922@hotmail.com>
2017-08-09 00:48:28 +0800

    GameofLife

--- LeetCode/src/GameofLife.java
+++ LeetCode/src/GameofLife.java
@@ -2,9 +2,11 @@
 
 289. Game of Life
 
-According to the Wikipedia's article: "The Game of Life, also known simply as Life, is a cellular automaton devised by the British mathematician John Horton Conway in 1970."
+According to the Wikipedia's article: "The Game of Life, also known simply as Life,
+is a cellular automaton devised by the British mathematician John Horton Conway in 1970."
 
-Given a board with m by n cells, each cell has an initial state live (1) or dead (0). Each cell interacts with its eight neighbors (horizontal, vertical, diagonal) using the following four rules (taken from the above Wikipedia article):
+Given a board with m by n cells, each cell has an initial state live (1) or dead (0).
+Each cell interacts with its eight neighbors (horizontal, vertical, diagonal) using the following four rules (taken from the above Wikipedia article):
 
 
 1.Any live cell with fewer than two live neighbors dies, as if caused by under-population.
@@ -17,10 +19,15 @@ Write a function to compute the next state (after one update) of the board given
 
 Follow up:
 
-1.Could you solve it in-place? Remember that the board needs to be updated at the same time: You cannot update some cells first and then use their updated values to update other cells.
-2.In this question, we represent the board using a 2D array. In principle, the board is infinite, which would cause problems when the active area encroaches the border of the array. How would you address these problems?
+1.Could you solve it in-place? Remember that the board needs to be updated at the same time:
+You cannot update some cells first and then use their updated values to update other cells.
+2.In this question, we represent the board using a 2D array. In principle, the board is infinite,
+which would cause problems when the active area encroaches the border of the array. How would you address these problems?
 
 
 */
 public class GameofLife {
+    public void gameOfLife(int[][] board) {
+
+    }
 }
