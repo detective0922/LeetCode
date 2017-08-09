d70be197989b86174d621d8c937c9ef7df3d4b45
detective0922 <detective0922@hotmail.com>
2017-08-09 19:44:57 +0800

    GameofLife

--- LeetCode/src/GameofLife.java
+++ LeetCode/src/GameofLife.java
@@ -30,10 +30,14 @@ public class GameofLife {
     public void gameOfLife(int[][] board) {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
-                
+                int liveCells = liveNeighbor(board, i, j);
             }
             
         }
+
+    }
+
+    private int liveNeighbor(int[][] board, int i, int j) {
 
     }
 }
