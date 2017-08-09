0f6bf4d9eb5df188306d0461c45d8e520d9a08db
detective0922 <detective0922@hotmail.com>
2017-08-10 01:20:57 +0800

    PerfectSquares

--- LeetCode/src/PerfectSquares.java
+++ LeetCode/src/PerfectSquares.java
@@ -11,9 +11,9 @@ public class PerfectSquares {
     public int numSquares(int n) {
         int[] dp = new int[n + 1];
         dp[0] = 0;
-        for (int i = 1; i < n; i++) {
+        for (int i = 1; i <= n; i++) {
             dp[i] = Integer.MAX_VALUE;
-            for (int j = 1; j * j < n; j++) {
+            for (int j = 1; j * j <= i; j++) {
                 dp[i] = Math.min(dp[i], dp[n - j * j] + 1);
             }
         }
