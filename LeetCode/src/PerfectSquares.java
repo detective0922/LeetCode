82a91bd3d7ed7be2207ce84693646b33b6e0c425
detective0922 <detective0922@hotmail.com>
2017-08-10 01:04:38 +0800

    PerfectSquares

--- LeetCode/src/PerfectSquares.java
+++ LeetCode/src/PerfectSquares.java
@@ -9,15 +9,15 @@ For example, given n = 12, return 3 because 12 = 4 + 4 + 4; given n = 13, return
 
 public class PerfectSquares {
     public int numSquares(int n) {
-        int[] dp = new int[n];
+        int[] dp = new int[n + 1];
         dp[0] = 0;
         for (int i = 1; i < n; i++) {
             int min = Integer.MAX_VALUE;
             for (int j = 1; j * j < n; j++) {
-                min = Math.min(min, dp[n - j*j] + 1);
+                min = Math.min(min, dp[n - j * j] + 1);
             }
             dp[i] = min;
         }
-        return dp[n-1];
+        return dp[n];
     }
 }
