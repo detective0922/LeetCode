5162d23b20e6ab8d6e6f9df4f87b10376910a30b
detective0922 <detective0922@hotmail.com>
2017-08-12 01:48:47 +0800

    ContainerWithMostWater

--- LeetCode/src/ContainerWithMostWater.java
+++ LeetCode/src/ContainerWithMostWater.java
@@ -15,11 +15,11 @@ public class ContainerWithMostWater {
         int low = 0;
         int high = height.length - 1;
         while (low < high) {
-            maxArea = Math.max(maxArea, (high - low) * Math.max(height[low], height[high]));
+            maxArea = Math.max(maxArea, (high - low) * Math.min(height[low], height[high]));
             if (height[low] <= height[high]) {
                 low++;
             } else {
-
+                high--;
             }
         }
         return maxArea;
