d6293e6c43578c3c8c6c2b0f6808c9c5a8baf664
detective0922 <detective0922@hotmail.com>
2017-08-19 00:55:26 +0800

    SubsetsII

--- LeetCode/src/SubsetsII.java
+++ LeetCode/src/SubsetsII.java
@@ -21,28 +21,25 @@ For example,
 */
 public class SubsetsII {
     public static void main(String[] args) {
-        System.out.println(new SubsetsII().subsetsWithDup(new int[]{0}));
+        System.out.println(new SubsetsII().subsetsWithDup(new int[]{1,2,2}));
     }
 
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         List<List<Integer>> subsets = new ArrayList<List<Integer>>();
         List<Integer> subset = new ArrayList<Integer>();
-        List<Integer> sums = new ArrayList<Integer>();
-        generateSubsets(subsets, subset, nums, 0, sums, 0);
+        generateSubsets(subsets, subset, nums, 0);
         return subsets;
     }
 
-    private void generateSubsets(List<List<Integer>> subsets, List<Integer> subset, int[] nums, int start, List<Integer> sums, int sum) {
-        if (!sums.contains(sum)) {
-            subsets.add(new ArrayList<Integer>(subset));
-            sums.add(sum);
-        }
+    private void generateSubsets(List<List<Integer>> subsets, List<Integer> subset, int[] nums, int start) {
+        subsets.add(new ArrayList<Integer>(subset));
         for (int i = start; i < nums.length; i++) {
-            sum += nums[i];
+            if (i > 1 && nums[i] == nums[i-1]) {
+                continue;
+            }
             subset.add(nums[i]);
-            generateSubsets(subsets, subset, nums, i + 1, sums, sum);
+            generateSubsets(subsets, subset, nums, i + 1);
             subset.remove(subset.size() - 1);
-            sum -= nums[i];
         }
     }
 }
