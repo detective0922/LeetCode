49ebb3e2dfdd20017576c0a35fe857328c1a3212
detective0922 <detective0922@hotmail.com>
2017-08-24 20:39:28 +0800

    SetMatrixZeroes

--- LeetCode/src/SetMatrixZeroes.java
+++ LeetCode/src/SetMatrixZeroes.java
@@ -28,6 +28,9 @@ public class SetMatrixZeroes {
     }
 
     private void fillZero(int[][] matrix, int row, int col) {
+        for (int i = 0; i < matrix[0].length; i++) {
+            matrix[row][i] = 0;
+        }
 
     }
 }
