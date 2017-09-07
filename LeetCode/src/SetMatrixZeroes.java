f3640dc60c6cd62c0114693ad928ce551fac0d62
detective0922 <detective0922@hotmail.com>
2017-09-08 02:32:05 +0800

    SetMatrixZeroes

--- LeetCode/src/SetMatrixZeroes.java
+++ LeetCode/src/SetMatrixZeroes.java
@@ -24,23 +24,40 @@ public class SetMatrixZeroes {
         if (matrix == null) {
             return;
         }
-        boolean[] rowZeros = new boolean[matrix.length];
-        boolean[] colZeros = new boolean[matrix[0].length];
+        boolean firstRow = false;
+        boolean firstCol = false;
 
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
                 if (matrix[i][j] == 0) {
-                    rowZeros[i] = true;
-                    colZeros[j] = true;
+                    if (i == 0) {
+                        firstRow = true;
+                    }
+                    if (j == 0) {
+                        firstCol = true;
+                    }
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
 
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                if(rowZeros[i] || colZeros[j]) {
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[0].length; j++) {
+                if(matrix[i][0] == 0 || matrix[0][j] == 0) {
                     matrix[i][j] = 0;
                 }
+            }
+        }
+        if (firstRow) {
+            for (int i = 0; i < matrix[0].length; i++) {
+                matrix[0][i] = 0;
+            }
+        }
+
+        if (firstCol) {
+            for (int i = 0; i < matrix.length; i++) {
+                matrix[i][0] = 0;
             }
         }
     }
