63b9249e3961d07f61e34043122e6d5e678db25c
detective0922 <detective0922@hotmail.com>
2017-09-07 21:04:49 +0800

    SetMatrixZeroes

--- LeetCode/src/SetMatrixZeroes.java
+++ LeetCode/src/SetMatrixZeroes.java
@@ -23,6 +23,11 @@ public class SetMatrixZeroes {
             return;
         }
         int[][] copyMatrix = new int[matrix.length][matrix[0].length];
+        for (int i = 0; i < copyMatrix.length; i++) {
+            for (int j = 0; j < copyMatrix[0].length; j++) {
+                copyMatrix[i][j] = -1;
+            }
+        }
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
                 if (matrix[i][j] == 0) {
