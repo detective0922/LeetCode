13712b640852a5082ca3dc65e4716736a5b5b433
detective0922 <detective0922@hotmail.com>
2017-10-16 17:02:38 +0800

    WiggleSubsequence

--- LeetCode/src/WiggleSubsequence.java
+++ LeetCode/src/WiggleSubsequence.java
@@ -34,6 +34,9 @@ Follow up:
 public class WiggleSubsequence {
     public int wiggleMaxLength(int[] nums) {
         for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < nums[i+1]) {
+
+            }
             
         }
 
