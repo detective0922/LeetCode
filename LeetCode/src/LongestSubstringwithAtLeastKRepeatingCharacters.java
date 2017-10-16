105a62020f04df09fd63aa858427848c8391af6d
detective0922 <detective0922@hotmail.com>
2017-10-16 14:49:30 +0800

    LongestSubstringwithAtLeastKRepeatingCharacters

--- LeetCode/src/LongestSubstringwithAtLeastKRepeatingCharacters.java
+++ LeetCode/src/LongestSubstringwithAtLeastKRepeatingCharacters.java
@@ -26,7 +26,7 @@ The longest substring is "ababb", as 'a' is repeated 2 times and 'b' is repeated
 */
 public class LongestSubstringwithAtLeastKRepeatingCharacters {
     public static void main(String[] args) {
-        System.out.println(new LongestSubstringwithAtLeastKRepeatingCharacters().longestSubstring("ababbc", 2));
+        System.out.println(new LongestSubstringwithAtLeastKRepeatingCharacters().longestSubstring("a", 1));
     }
 
     public int longestSubstring(String s, int k) {
@@ -35,19 +35,19 @@ public class LongestSubstringwithAtLeastKRepeatingCharacters {
     }
 
     private int helper(char[] chars, int start, int end, int k) {
-        if (end <= start || (end-start) < k) {
+        if (end < start || (end - start + 1) < k) {
             return 0;
         }
         char[] sMap = new char[26];
-        for (int i = start; i < end; i++) {
+        for (int i = start; i <= end; i++) {
             sMap[chars[i] - 'a']++;
         }
         for (int i = 0; i < sMap.length; i++) {
             if (sMap[i] > 0 && sMap[i] < k) {
                 for (int j = start; j <= end; j++) {
                     if (chars[j] == i + 'a') {
-                        int left = helper(chars, start, j-1, k);
-                        int right = helper(chars, j+1, end, k);
+                        int left = helper(chars, start, j - 1, k);
+                        int right = helper(chars, j + 1, end, k);
                         return Math.max(left, right);
                     }
                 }
